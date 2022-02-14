2889e650bbc78f98d443f1fb95a9db1605cb0d0c
zaiin-u <zainarshad.17@hotmail.com>
2022-02-14 13:30:55 +0500

    1st-five-sections

---
+++ script.js
@@ -0,0 +1,27 @@
+// Remember, we're gonna use strict mode in all scripts now!
+"use strict";
+/*
+const printforecast = function (t1) {
+  let new_one= "";
+  for (let i = 0; i < t1.length; i++) {
+    new_one += `${t1[i]}C in ${i + 1} day...`;
+    //`${t1[i]}C in ${i} day`
+  }
+  console.log("..." + new_one);
+};
+printforecast([12, 13, 14]);
+*/
+//to create array
+
+/*const printforecast = function (t1) {
+  let new_one = "";
+  let array1 = [];
+  for (let i = 0; i < t1.length; i++) {
+    new_one = `${t1[i]}C in ${i + 1} day`;
+    //`${t1[i]}C in ${i} day`
+    array1.push(new_one);
+  }
+  console.log(array1);
+};
+printforecast([12, 13, 14]);
+*/
